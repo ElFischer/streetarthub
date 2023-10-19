1f5a8f1787ec007ba4b6240e16f96a56560a0039
FischerEnrique <e.fischer@8erlin.de>
2023-10-19 14:43:49 +0200

    footer navigation social

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -11,95 +11,6 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
     <>
       {/* Footer */}
       <footer className={cn(className, 'mt-32 sm:mt-40')}>
-        <h2 id="footer-heading" className="sr-only">
-          Footer
-        </h2>
-        {/* <div className="container mx-auto pb-8 border-t">
-          <div className=" pt-20 xl:grid xl:grid-cols-3 xl:gap-8">
-            <div className="grid grid-cols-2 gap-8 xl:col-span-2">
-              <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold leading-6">Solutions</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.solutions.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-10 md:mt-0">
-                  <h3 className="text-sm font-semibold leading-6">Support</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.support.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold leading-6">Company</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.company.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-10 md:mt-0">
-                  <h3 className="text-sm font-semibold leading-6">Legal</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.legal.map((item) => (
-                      <li key={item.name}>
-                        <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="mt-10 xl:mt-0">
-              <h3 className="text-sm font-semibold leading-6">Subscribe to our newsletter</h3>
-              <p className="mt-2 text-sm leading-6 text-gray-600">
-                The latest news, articles, and resources, sent to your inbox weekly.
-              </p>
-              <form className="mt-6 sm:flex sm:max-w-md">
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  autoComplete="email"
-                  required
-                  className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
-                  placeholder="Enter your email"
-                />
-                <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                  <button
-                    type="submit"
-                    className="flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div> */}
         <div className="container-fluid flex flex-col items-center justify-between gap-4 py-10 md:h-20 md:flex-row md:py-0 ">
           <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
             {/* <Icons.logo /> */}
@@ -139,7 +50,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
               {footerNavigation.social.map((item) => {
                 const Icon = Icons[item.icon || "arrowRight"]
                 return (
-                  <Link key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500 inline-flex items-center justify-center">
+                  <Link key={item.name} title={'streetarthub on ' + item.name} href={item.href} target="_blank" className="text-gray-400 hover:text-gray-500 inline-flex items-center justify-center">
                     <span className="sr-only">{item.name}</span>
                     <Icon className="h-6 w-6" />
                   </Link>
