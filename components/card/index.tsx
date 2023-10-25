c3158b119b83e5f42dc27a02da6ba3d367f71b5a
FischerEnrique <e.fischer@8erlin.de>
2023-10-25 08:07:03 +0200

    AspectRatio Component

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/button";
 import { Icons } from "../icons";
 import { getDownloadURL, ref } from "firebase/storage";
 import { storage } from "@/lib/firebase";
+import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/lib/firebase";
@@ -38,77 +39,40 @@ export default function Card({ id, title, image, description, count, type, sourc
 
     return (
         <>
-            {cover ? (
-                <article className={`group relative `}>
-                    <div className="group relative">
-                        <div className={`relative overflow-hidden rounded-lg bg-gray-200 group-hover:opacity-75`} style={{ paddingBottom: `${(cover[0].height / cover[0].width) * 100}%` }}>
-                            <Link href={`${type ? type : '/art'}/${id}`}>
-                                <NextImage
-                                    src={`https://firebasestorage.googleapis.com/v0/b/nuxtsah.appspot.com/o/art%2F@s_500_${image}?alt=media`}
-                                    alt={title}
-                                    fill={true}
-                                    sizes="450px"
-                                    className="object-cover object-center"
-                                    priority={true}
-                                    placeholder='blur'
-                                    blurDataURL={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=`}
-                                />
-                                <span className="sr-only">View Article</span>
+            <article className={`group relative `}>
+                <div className="group relative">
+                    <AspectRatio ratio={cover ? cover[0].width / cover[0].height : 5 / 4} className="bg-muted group-hover:opacity-75">
+                        <Link href={`${type ? type : '/art'}/${id}`}>
+                            <NextImage
+                                src={`https://firebasestorage.googleapis.com/v0/b/nuxtsah.appspot.com/o/art%2F@s_500_${image}?alt=media`}
+                                alt={title}
+                                fill={true}
+                                sizes="450px"
+                                className="rounded-lg object-cover object-center"
+                                priority={true}
+                                placeholder='blur'
+                                blurDataURL={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=`}
+                            />
+                            <span className="sr-only">View Article</span>
+                        </Link>
+                    </AspectRatio>
+                    <div className="absolute top-0 right-0 p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
+                        <Button variant={'ghost'} size="icon" className="rounded-md bg-white bg-opacity-75 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter"><Icons.heart className="h-4 w-4" /></Button>
+                    </div>
+                    {source && (
+                        <div className="absolute bottom-0 p-4 transition-opacity duration-150 ease-in-out opacity-0 group-hover:opacity-100" aria-hidden="true">
+                            <Link href={source} target="_blank">
+                                <Button variant={'ghost'} size="icon" className="rounded-md bg-white bg-opacity-75 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter"><Icons.link className="h-4 w-4" /></Button>
                             </Link>
                         </div>
-                        <div className="absolute top-0 right-0 p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
-                            <Button variant={'ghost'} size="icon" className="rounded-md bg-white bg-opacity-75 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter"><Icons.heart className="h-4 w-4" /></Button>
-                        </div>
-                        {source && (
-                            <div className="absolute bottom-0 p-4 transition-opacity duration-150 ease-in-out opacity-0 group-hover:opacity-100" aria-hidden="true">
-                                <Link href={source} target="_blank">
-                                    <Button variant={'ghost'} size="icon" className="rounded-md bg-white bg-opacity-75 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter"><Icons.link className="h-4 w-4" /></Button>
-                                </Link>
-                            </div>
-                        )}
-                    </div>
-                    <div className="mt-4 flex items-center justify-between space-x-8 font-medium text-gray-900">
-                        <Link href={`${type ? type : '/art'}/${id}`} className="text-sm font-semibold leading-none truncate w-full max-w-[calc(100%-2rem)]">
-                            {title}
-                        </Link>
-                    </div>
-                </article >
-            ) : (
-                <article className={`group relative`}>
-                    <div className="group relative">
-                        <div className="aspect-h-4 aspect-w-4 overflow-hidden rounded-lg bg-gray-200 group-hover:opacity-75">
-                            <Link href={`${type ? type : '/art'}/${id}`}>
-                                <NextImage
-                                    src={`https://firebasestorage.googleapis.com/v0/b/nuxtsah.appspot.com/o/art%2F@s_500_${image}?alt=media`}
-                                    alt={title}
-                                    fill={true}
-                                    sizes="450px"
-                                    className="object-cover object-center"
-                                    priority={true}
-                                    placeholder='blur'
-                                    blurDataURL={`data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=`}
-                                />
-                                <span className="sr-only">View Article</span>
-                            </Link>
-                        </div>
-                        <div className="absolute top-0 right-0 p-4 opacity-0 group-hover:opacity-100" aria-hidden="true">
-                            <Button variant={'ghost'} size="icon" className="rounded-md bg-white bg-opacity-75 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter"><Icons.heart className="h-4 w-4" /></Button>
-                        </div>
-                        {source && (
-                            <div className="absolute bottom-0 p-4 transition-opacity duration-150 ease-in-out opacity-0 group-hover:opacity-100" aria-hidden="true">
-                                <Link href={source} target="_blank">
-                                    <Button variant={'ghost'} size="icon" className="rounded-md bg-white bg-opacity-75 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter"><Icons.link className="h-4 w-4" /></Button>
-                                </Link>
-                            </div>
-                        )}
-                    </div>
-                    <div className="mt-4 flex items-center justify-between space-x-8 font-medium text-gray-900">
-                        <Link href={`${type ? type : '/art'}/${id}`} className="text-sm font-semibold leading-none truncate w-full max-w-[calc(100%-2rem)]">
-                            {title}
-                        </Link>
-                    </div>
-                </article>
-            )}
+                    )}
+                </div>
+                <div className="mt-4 flex items-center justify-between space-x-8 font-medium text-gray-900">
+                    <Link href={`${type ? type : '/art'}/${id}`} className="text-sm font-semibold leading-none truncate w-full max-w-[calc(100%-2rem)]">
+                        {title}
+                    </Link>
+                </div>
+            </article >
         </>
     )
 }
