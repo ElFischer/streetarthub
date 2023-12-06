ae17df11faefd41d3ba5a3ee4e6c77813d1f9e6b
FischerEnrique <e.fischer@8erlin.de>
2023-12-06 10:04:55 +0100

    style: padding mobile card container

--- app/(corporate)/(categories)/artists/[...artistId]/page.tsx
+++ app/(corporate)/(categories)/artists/[...artistId]/page.tsx
@@ -15,8 +15,8 @@ export default async function ArtistPage({ params }: any) {
     const artist = await getPostFromParams(params)
 
     return (
-        <div className="container">
-            <div className="py-16 lg:max-w-2xl">
+        <div className="container px-0 sm:px-8">
+            <div className="py-16 lg:max-w-2xl sm:px-0 px-8">
                 <h1 className="font-heading text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight max-w-lg md:max-w-2xl">{artist?.name}</h1>
                 {/* <p className="mt-6 text-base leading-7 text-muted-foreground">
           Discover handpicked collections, both from our team and our creative users, meticulously moderated to ensure an inspiring journey through the world of Streetart. Dive into these thoughtfully crafted selections and let your Streetart exploration begin.
