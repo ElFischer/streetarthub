e01a5fe2b975c5d442256078995b82903ddb9cbe
FischerEnrique <e.fischer@8erlin.de>
2023-12-09 06:54:00 +0100

    refactor(skeleton): add feed-skeleton component

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -12,4 +12,19 @@ function Skeleton({
   )
 }
 
-export { Skeleton }
+const FeedSkeleton = () => {
+  const skeletons = Array.from({ length: 9 });
+
+  return (
+    <div className="my-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {skeletons.map((_, index) => (
+        <div key={index} className="space-y-3">
+          <Skeleton className="h-60" />
+          <Skeleton className="h-4 w-4/5" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export { Skeleton, FeedSkeleton }
